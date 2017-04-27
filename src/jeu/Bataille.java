84158d60e4d37e956480a52557981ddbf4118d43
Madjid <madjid.bouchair@gmail.com>
2017-04-27 11:06:00 +0200

    Swing TO DO : Afficher les cartes "coeur"

--- src/jeu/Bataille.java
+++ src/jeu/Bataille.java
@@ -1,8 +1,9 @@
+package jeu;
 
 public class Bataille {
 	
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws InterruptedException {
 
 		Jeu bataille = new Jeu();
 		bataille.Jeu();
